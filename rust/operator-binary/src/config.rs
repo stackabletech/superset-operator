28c97be4d6db46317846ef96a2d364a372185264
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-03-22 08:16:03 +0100

    Use indoc for multiline strings

--- rust/operator-binary/src/config.rs
+++ rust/operator-binary/src/config.rs
@@ -1,3 +1,4 @@
+use indoc::{formatdoc, indoc};
 use stackable_superset_crd::authentication::{
     AuthenticationClass, AuthenticationClassCaCert, AuthenticationClassLdap,
     AuthenticationClassProtocol, AuthenticationClassTls,
@@ -10,22 +11,22 @@ pub fn compute_superset_config(
 ) -> String {
     // We don't calculate the secrets here directly, as the operator should not be able to see the actual credentials.
     // Instead we add a env-var which gets it's value from the secret with the credentials and read that with Python.
-    let mut config = r#"
-# Common configs
-import os
-from superset.stats_logger import StatsdStatsLogger
-from flask_appbuilder.security.manager import (
-    AUTH_DB,
-    AUTH_LDAP,
-    AUTH_OAUTH,
-    AUTH_OID,
-    AUTH_REMOTE_USER
-)
+    let mut config = indoc! {r#"
+        # Common configs
+        import os
+        from superset.stats_logger import StatsdStatsLogger
+        from flask_appbuilder.security.manager import (
+            AUTH_DB,
+            AUTH_LDAP,
+            AUTH_OAUTH,
+            AUTH_OID,
+            AUTH_REMOTE_USER
+        )
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-STATS_LOGGER = StatsdStatsLogger(host='0.0.0.0', port=9125)
-"#
+        SECRET_KEY = os.environ.get('SECRET_KEY')
+        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        STATS_LOGGER = StatsdStatsLogger(host='0.0.0.0', port=9125)
+    "#}
     .to_string();
 
     if let Some(authentication_method) = authentication_method {
@@ -57,24 +58,22 @@ fn append_ldap_config(
     let authentication_class_name = &authentication_method.authentication_class;
 
     config.push_str(
-        format!(
-            r#"
-# Authentication configs
-AUTH_TYPE = AUTH_LDAP
-AUTH_LDAP_SERVER = "{}{}:{}"
+        formatdoc! {r#"
+                # Authentication configs
+                AUTH_TYPE = AUTH_LDAP
+                AUTH_LDAP_SERVER = "{}{}:{}"
 
-AUTH_LDAP_SEARCH = "{}"
-AUTH_LDAP_UID_FIELD = "{}"
-AUTH_LDAP_GROUP_FIELD = "{}"
-AUTH_LDAP_FIRSTNAME_FIELD = "{}"
-AUTH_LDAP_LASTNAME_FIELD = "{}"
-AUTH_LDAP_EMAIL_FIELD = "{}"
+                AUTH_LDAP_SEARCH = "{}"
+                AUTH_LDAP_UID_FIELD = "{}"
+                AUTH_LDAP_GROUP_FIELD = "{}"
+                AUTH_LDAP_FIRSTNAME_FIELD = "{}"
+                AUTH_LDAP_LASTNAME_FIELD = "{}"
+                AUTH_LDAP_EMAIL_FIELD = "{}"
 
-AUTH_USER_REGISTRATION = "{}"
-AUTH_USER_REGISTRATION_ROLE = "{}"
-AUTH_ROLES_SYNC_AT_LOGIN = "{}"
-
-"#,
+                AUTH_USER_REGISTRATION = "{}"
+                AUTH_USER_REGISTRATION_ROLE = "{}"
+                AUTH_ROLES_SYNC_AT_LOGIN = "{}"
+            "#,
             match ldap.tls {
                 None => "ldap://",
                 Some(_) => "ldaps://",
@@ -106,7 +105,7 @@ AUTH_ROLES_SYNC_AT_LOGIN = "{}"
                     .map(|extra| extra.roles_sync_at_login)
                     .unwrap_or_else(stackable_superset_crd::default_roles_sync_at_login)
             ),
-        )
+        }
         .as_str(),
     );
 
@@ -119,17 +118,12 @@ AUTH_ROLES_SYNC_AT_LOGIN = "{}"
     // app.config.setdefault("AUTH_LDAP_TLS_CERTFILE", "")
     // app.config.setdefault("AUTH_LDAP_TLS_KEYFILE", "")
     match &ldap.tls {
-        None => config.push_str(
-            r#"
-AUTH_LDAP_USE_TLS = False
-"#,
-        ),
+        None => config.push_str("AUTH_LDAP_USE_TLS = False\n"),
         Some(AuthenticationClassTls::Insecure {}) => {
-            config.push_str(
-                r#"
-AUTH_LDAP_USE_TLS = False # Strangely we don't want True here because it will use TLS and we need to use SSL.
-AUTH_LDAP_ALLOW_SELF_SIGNED = True
-"#,
+            config.push_str(indoc! {r#"
+                AUTH_LDAP_USE_TLS = False # Strangely we don't want True here because it will use TLS and we need to use SSL.
+                AUTH_LDAP_ALLOW_SELF_SIGNED = True
+            "#}
             );
         }
         Some(AuthenticationClassTls::ServerVerification(server_verification)) => {
@@ -147,26 +141,20 @@ AUTH_LDAP_ALLOW_SELF_SIGNED = True
                     mutual_verification.secret_class.to_string(),
                 ),
             );
-            config.push_str(
-                format!(
-                    r#"
-AUTH_LDAP_TLS_CERTFILE = "/certificates/{authentication_class_name}-tls-certificate/tls.crt"
-AUTH_LDAP_TLS_KEYFILE = "/certificates/{authentication_class_name}-tls-certificate/tls.key"
-"#
-                )
-                .as_str(),
-            );
+            config.push_str(formatdoc! {r#"
+                    AUTH_LDAP_TLS_CERTFILE = "/certificates/{authentication_class_name}-tls-certificate/tls.crt"
+                    AUTH_LDAP_TLS_KEYFILE = "/certificates/{authentication_class_name}-tls-certificate/tls.key"
+                "#}
+                .as_str());
         }
     }
 
     if ldap.bind_credentials.is_some() {
         config.push_str(
-            format!(
-                r#"
-AUTH_LDAP_BIND_USER = open('/secrets/{authentication_class_name}-bind-credentials/user').read()
-AUTH_LDAP_BIND_PASSWORD = open('/secrets/{authentication_class_name}-bind-credentials/password').read()
-"#
-            )
+            formatdoc! {r#"
+                    AUTH_LDAP_BIND_USER = open('/secrets/{authentication_class_name}-bind-credentials/user').read()
+                    AUTH_LDAP_BIND_PASSWORD = open('/secrets/{authentication_class_name}-bind-credentials/password').read()
+                "#}
                 .as_str(),
         );
     }
@@ -178,11 +166,11 @@ fn append_server_ca_cert(
     server_ca_cert: &AuthenticationClassCaCert,
 ) {
     config.push_str(
-        r#"
-AUTH_LDAP_USE_TLS = False # Strangely we don't want True here because it will use TLS and we need to use SSL.
-AUTH_LDAP_ALLOW_SELF_SIGNED = False
-AUTH_LDAP_TLS_DEMAND = True
-"#,
+        indoc! {r#"
+            AUTH_LDAP_USE_TLS = False # Strangely we don't want True here because it will use TLS and we need to use SSL.
+            AUTH_LDAP_ALLOW_SELF_SIGNED = False
+            AUTH_LDAP_TLS_DEMAND = True
+        "#},
     );
     match server_ca_cert {
         AuthenticationClassCaCert::Path(path) => {
@@ -191,12 +179,7 @@ AUTH_LDAP_TLS_DEMAND = True
         AuthenticationClassCaCert::Configmap(_)
         | AuthenticationClassCaCert::Secret(_)
         | AuthenticationClassCaCert::SecretClass(_) => {
-            config.push_str(
-                format!(
-                    "AUTH_LDAP_TLS_CACERTFILE = \"/certificates/{authentication_class_name}-tls-certificate/ca.crt\"\n"
-                )
-                    .as_str(),
-            );
+            config.push_str(format!("AUTH_LDAP_TLS_CACERTFILE = \"/certificates/{authentication_class_name}-tls-certificate/ca.crt\"\n").as_str());
         }
     }
 }
