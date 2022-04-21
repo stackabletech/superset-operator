bc32d74fb3cd43493665524cd591e1163eafc139
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-04-21 11:13:50 +0200

    Add LDAP to CRD

--- rust/crd/src/lib.rs
+++ rust/crd/src/lib.rs
@@ -94,7 +94,66 @@ pub struct SupersetClusterSpec {
     #[serde(default)]
     pub load_examples_on_init: Option<bool>,
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub authentication_config: Option<SupersetClusterAuthenticationConfig>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub nodes: Option<Role<SupersetConfig>>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SupersetClusterAuthenticationConfig {
+    pub methods: Vec<SupersetClusterAuthenticationConfigMethod>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SupersetClusterAuthenticationConfigMethod {
+    /// Name of the AuthenticationClass used to authenticate the users
+    pub authentication_class: String,
+    /// Additional LDAP settings.
+    /// Can only be specified when the specified AuthenticationClass uses the LDAP protocol
+    /// See [Flask LDAP documentation](https://flask-appbuilder.readthedocs.io/en/latest/security.html#authentication-ldap)
+    pub ldap_extras: Option<SupersetClusterAuthenticationConfigLdapExtras>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+/// Additional configs to LDAP Authentication in Superset. See <https://flask-appbuilder.readthedocs.io/en/latest/security.html#authentication-ldap>
+pub struct SupersetClusterAuthenticationConfigLdapExtras {
+    /// Allow users who are not already in the FAB DB.
+    /// Gets mapped to `AUTH_USER_REGISTRATION`
+    #[serde(default = "default_user_registration")]
+    pub user_registration: bool,
+
+    /// This role will be given in addition to any AUTH_ROLES_MAPPING.
+    /// Gets mapped to `AUTH_USER_REGISTRATION_ROLE`
+    #[serde(default = "default_user_registration_role")]
+    pub user_registration_role: String,
+
+    /// If we should replace ALL the user's roles each login, or only on registration.
+    /// Gets mapped to `AUTH_ROLES_SYNC_AT_LOGIN`
+    #[serde(default = "default_sync_roles_at")]
+    pub sync_roles_at: LdapRolesSyncMoment,
+}
+
+pub fn default_user_registration() -> bool {
+    true
+}
+
+pub fn default_user_registration_role() -> String {
+    "Public".to_string()
+}
+
+/// Matches Flask's default mode of syncing at registration
+pub fn default_sync_roles_at() -> LdapRolesSyncMoment {
+    LdapRolesSyncMoment::Registration
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
+#[serde(rename_all = "PascalCase")]
+pub enum LdapRolesSyncMoment {
+    Registration,
+    Login,
 }
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
@@ -162,7 +221,7 @@ impl Configuration for SupersetConfig {
 
     fn compute_cli(
         &self,
-        _resource: &Self::Configurable,
+        _cluster: &Self::Configurable,
         _role_name: &str,
     ) -> Result<BTreeMap<String, Option<String>>, ConfigError> {
         Ok(BTreeMap::new())
@@ -170,7 +229,7 @@ impl Configuration for SupersetConfig {
 
     fn compute_files(
         &self,
-        _resource: &Self::Configurable,
+        _cluster: &Self::Configurable,
         _role_name: &str,
         file: &str,
     ) -> Result<BTreeMap<String, Option<String>>, ConfigError> {
@@ -212,7 +271,7 @@ impl SupersetCluster {
 }
 
 /// A reference to a [`SupersetCluster`]
-#[derive(Clone, Default, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
+#[derive(Clone, Debug, Default, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SupersetClusterRef {
     #[serde(default, skip_serializing_if = "Option::is_none")]
