9453b9b7b6e5d4088a844abf5b97f9e8102393cf
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-05-13 17:14:19 +0000

    First round of migration of integration-tests (#198)
    ## Description
    
    Well that was easier than expected.
    LDAP tests will follow
    Run with `scripts/run_tests.sh --parallel 4`

---
+++ tests/templates/kuttl/smoke/metrics.py
@@ -0,0 +1,15 @@
+import requests
+import time
+
+# Create and increase the superset_welcome counter
+web_ui_response = requests.get("http://superset-node-default:8088/")
+assert web_ui_response.status_code == 200, "Web UI could not be reached successfully."
+
+# Wait for the counter to be consumed by the statsd-exporter
+time.sleep(2)
+
+metrics_response = requests.get("http://superset-node-default:9102/metrics")
+assert metrics_response.status_code == 200, "Metrics could not be retrieved."
+
+assert "superset_welcome" in metrics_response.text, \
+    "The metrics do not contain the superset_welcome counter."
