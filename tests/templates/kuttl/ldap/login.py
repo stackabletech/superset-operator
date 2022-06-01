55394b457bf73ab7088a96433e180b686f8d5b2c
Siegfried Weber <mail@siegfriedweber.net>
2022-06-01 08:10:22 +0200

    Fix linter warning

--- tests/templates/kuttl/ldap/login.py
+++ tests/templates/kuttl/ldap/login.py
@@ -2,17 +2,17 @@ import requests
 import sys
 import logging
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     result = 0
 
     log_level = 'DEBUG'  # if args.debug else 'INFO'
     logging.basicConfig(level=log_level, format='%(asctime)s %(levelname)s: %(message)s', stream=sys.stdout)
 
-    http_code = requests.post(f"http://superset-with-ldap-node-default:8088/api/v1/security/login", json={
-        "username": "integrationtest",
-        "password": "integrationtest",
-        "provider": "ldap",
-        "refresh": "true",
+    http_code = requests.post('http://superset-with-ldap-node-default:8088/api/v1/security/login', json={
+        'username': 'integrationtest',
+        'password': 'integrationtest',
+        'provider': 'ldap',
+        'refresh': 'true',
     }).status_code
     if http_code != 200:
         result = 1
