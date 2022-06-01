82b3020112ac4ef6bed86d4c1e09bdf54a78990f
Siegfried Weber <mail@siegfriedweber.net>
2022-06-01 13:38:33 +0000

    Create LDAP integration tests (#206)
    # Description
    
    Create LDAP integration tests
    
    
    
    Co-authored-by: Nikolaus Winter <mail@nikolaus-winter.de>

---
+++ tests/templates/kuttl/ldap/login.py
@@ -0,0 +1,20 @@
+import requests
+import sys
+import logging
+
+if __name__ == '__main__':
+    result = 0
+
+    log_level = 'DEBUG'  # if args.debug else 'INFO'
+    logging.basicConfig(level=log_level, format='%(asctime)s %(levelname)s: %(message)s', stream=sys.stdout)
+
+    http_code = requests.post('http://superset-with-ldap-node-default:8088/api/v1/security/login', json={
+        'username': 'integrationtest',
+        'password': 'integrationtest',
+        'provider': 'ldap',
+        'refresh': 'true',
+    }).status_code
+    if http_code != 200:
+        result = 1
+
+    sys.exit(result)
