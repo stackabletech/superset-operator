4f217d7fde5c29fec1e2c59eff5868dd3ba2f8e2
Nikolaus Winter <mail@nikolaus-winter.de>
2022-06-21 16:02:12 +0200

    removed CI files

--- .ci/integration-tests/hcloud-centos-8/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone -b "$GIT_BRANCH" https://github.com/stackabletech/superset-operator.git
-(cd superset-operator/ && ./scripts/run_tests.sh)
-exit_code=$?
-./operator-logs.sh superset > /target/superset-operator.log
-exit $exit_code
