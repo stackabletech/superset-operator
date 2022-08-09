01a5db6bc78ac7b524337c68fb63d02d78c383aa
Felix Hennig <felix.hennig@stackable.de>
2022-08-09 11:50:08 +0200

    expanded script

--- docs/modules/getting_started/examples/code/getting_started.sh
+++ docs/modules/getting_started/examples/code/getting_started.sh
@@ -42,8 +42,49 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 
 echo "Installing bitnami PostgreSQL"
 # tag::install-bitnami-psql[]
-helm install superset bitnami/postgresql \
+helm install --wait superset bitnami/postgresql \
     --set auth.username=superset \
     --set auth.password=superset \
     --set auth.database=superset
 # end::install-bitnami-psql[]
+
+echo "Creating credentials secret"
+# tag::apply-superset-credentials[]
+kubectl apply -f superset-credentials.yaml
+# end::apply-superset-credentials[]
+
+echo "Creating Superset cluster"
+# tag::apply-superset-cluster[]
+kubectl apply -f superset.yaml
+# end::apply-superset-cluster[]
+
+echo "Waiting on SupersetDB ..."
+# tag::wait-supersetdb[]
+kubectl wait supersetdb/simple-superset \
+  --for jsonpath='{.status.condition}'=Ready \
+  --timeout 300s
+# end::wait-supersetdb[]
+
+sleep 5
+
+echo "Wainting on superset StatefulSet ..."
+kubectl wait pod/simple-superset-node-default-0 \
+  --for condition=ready \
+  --timeout 60s
+
+echo "Starting port-forwarding of port 8088"
+# tag::port-forwarding[]
+kubectl port-forward service/simple-superset-external 8088 2>&1 >/dev/null &
+# end::port-forwarding[]
+PORT_FORWARD_PID=$!
+trap "kill $PORT_FORWARD_PID" EXIT
+sleep 5
+
+echo "Checking if web interface is reachable ..."
+return_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8088/login/)
+if [ "$return_code" == 200 ]; then
+  echo "Web interface reachable!"
+else
+  echo "Could not reach web interface."
+  exit 1
+fi
