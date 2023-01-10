d1de8b8c54a9d9c2d97da83b40d9c3deb2124b9e
Malte Sander <malte.sander.it@gmail.com>
2023-01-10 12:17:08 +0100

    removed deploy crd folder

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,12 +1,3 @@
-use stackable_operator::crd::CustomResourceExt;
-use stackable_superset_crd::druidconnection::DruidConnection;
-use stackable_superset_crd::supersetdb::SupersetDB;
-use stackable_superset_crd::SupersetCluster;
-
 fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    SupersetCluster::write_yaml_schema("../../deploy/crd/supersetcluster.crd.yaml").unwrap();
-    SupersetDB::write_yaml_schema("../../deploy/crd/supersetdb.crd.yaml").unwrap();
-    DruidConnection::write_yaml_schema("../../deploy/crd/druidconnection.crd.yaml").unwrap();
 }
