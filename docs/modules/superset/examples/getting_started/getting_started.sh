aaf5e7e7b6d8c4d3f415f5051f00da971f742fbb
Felix Hennig <felix.hennig@stackable.tech>
2023-03-08 10:08:05 +0100

    Added helm repo update

--- docs/modules/superset/examples/getting_started/getting_started.sh
+++ docs/modules/superset/examples/getting_started/getting_started.sh
@@ -22,6 +22,8 @@ echo "Adding 'stackable-dev' Helm Chart repository"
 # tag::helm-add-repo[]
 helm repo add stackable-dev https://repo.stackable.tech/repository/helm-dev/
 # end::helm-add-repo[]
+echo "Updating Helm repo"
+helm repo update
 echo "Installing Operators with Helm"
 # tag::helm-install-operators[]
 helm install --wait commons-operator stackable-dev/commons-operator --version 0.0.0-dev
