04409ec7a229bd804a10e71593bc6d3faeae46d9
Techassi <sascha.lautenschlaeger@stackable.tech>
2023-10-19 09:50:11 +0200

    Revert CRD changes

--- rust/crd/src/lib.rs
+++ rust/crd/src/lib.rs
@@ -26,7 +26,6 @@ use stackable_operator::{
     role_utils::{GenericRoleConfig, Role, RoleGroup, RoleGroupRef},
     schemars::{self, JsonSchema},
     status::condition::{ClusterCondition, HasStatusCondition},
-    time::Duration,
 };
 use std::collections::BTreeMap;
 use strum::{Display, EnumIter, EnumString, IntoEnumIterator};
@@ -299,10 +298,9 @@ pub struct SupersetConfig {
     /// Maximum time period a Superset request can take before timing out. This
     /// setting affects the maximum duration a query to an underlying datasource
     /// can take. If you get timeout errors before your query returns the result
-    /// you may need to increase this timeout. Corresponds to SUPERSET_WEBSERVER_TIMEOUT.
-    /// See <https://docs.stackable.tech/home/nightly/concepts/duration> for
-    /// more information on the duration format.
-    pub webserver_timeout: Option<Duration>,
+    /// you may need to increase this timeout. Corresponds to
+    /// SUPERSET_WEBSERVER_TIMEOUT.
+    pub webserver_timeout: Option<u32>,
 
     /// CPU and memory limits for Superset pods
     #[fragment_attrs(serde(default))]
