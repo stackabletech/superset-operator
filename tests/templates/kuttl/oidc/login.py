f088ffe784a25ef2aba737b1fba7d70c979fb3e5
Siegfried Weber <mail@siegfriedweber.net>
2024-01-24 19:51:54 +0100

    Use TLS for Keycloak in the OIDC test

--- tests/templates/kuttl/oidc/login.py
+++ tests/templates/kuttl/oidc/login.py
@@ -1,3 +1,5 @@
+# $NAMESPACE will be replaced with the namespace of the test case.
+
 import requests
 import sys
 import logging
@@ -14,11 +16,11 @@ session = requests.Session()
 login_page = session.get("http://superset-external:8088/login/keycloak?next=")
 
 assert login_page.ok, "Redirection from Superset to Keycloak failed"
-assert login_page.url.startswith("http://keycloak:8080/realms/superset/protocol/openid-connect/auth?response_type=code&client_id=superset"), \
+assert login_page.url.startswith("https://keycloak.$NAMESPACE.svc.cluster.local:8443/realms/test/protocol/openid-connect/auth?response_type=code&client_id=superset"), \
     "Redirection to the Keycloak login page expected"
 
 # Enter username and password into the Keycloak login page and click on "Sign In"
-login_page_html = BeautifulSoup(login_page.text, 'parser.html')
+login_page_html = BeautifulSoup(login_page.text, 'html.parser')
 authenticate_url = login_page_html.form['action']
 welcome_page = session.post(authenticate_url, data = {
     "username": "test",
