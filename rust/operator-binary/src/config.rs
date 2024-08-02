b2dff562cd3f073bbb222a91ca9b99315dfc2c15
Maxi Wittich <maximilian.wittich@stackable.tech>
2024-08-02 09:49:14 +0200

    Changing path to custom security manager

--- rust/operator-binary/src/config.rs
+++ rust/operator-binary/src/config.rs
@@ -24,7 +24,7 @@ pub const PYTHON_IMPORTS: &[&str] = &[
     "from flask_appbuilder.security.manager import (AUTH_DB, AUTH_LDAP, AUTH_OAUTH, AUTH_OID, AUTH_REMOTE_USER)",
     "from log_config import StackableLoggingConfigurator",
     // Custom logout manager to securely logout while using Keycloak SSO. Issue: https://github.com/apache/superset/issues/24713
-    "from security.CustomKeycloakSecurityManager import CustomSsoSecurityManager",
+    "from superset.security.CustomKeycloakSecurityManager import CustomSsoSecurityManager",
     ];
 
 pub fn add_superset_config(
