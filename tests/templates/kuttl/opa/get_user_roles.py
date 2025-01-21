75f19d2767137e28e5fc6a6304afa3f5c778324e
Benedikt Labrenz <benedikt@labrenz.org>
2025-01-21 14:27:02 +0100

    lint with ruff formatter

--- tests/templates/kuttl/opa/get_user_roles.py
+++ tests/templates/kuttl/opa/get_user_roles.py
@@ -5,32 +5,44 @@ import requests
 from bs4 import BeautifulSoup
 
 logging.basicConfig(
-    level='DEBUG',
-    format="%(asctime)s %(levelname)s: %(message)s",
-    stream=sys.stdout)
+    level="DEBUG", format="%(asctime)s %(levelname)s: %(message)s", stream=sys.stdout
+)
 
 namespace = sys.argv[1]
-expected_roles = set(sys.argv[2].split(','))
+expected_roles = set(sys.argv[2].split(","))
 
 session = requests.Session()
 
 # Click on "Login" in Superset
-login_page = session.get(f'http://superset-external.{namespace}.svc.cluster.local:8088/login/')
+login_page = session.get(
+    f"http://superset-external.{namespace}.svc.cluster.local:8088/login/"
+)
 assert login_page.status_code == 200
 
-login_page_html = BeautifulSoup(login_page.text, 'html.parser')
-csrf_token = login_page_html.find('input',{'id':'csrf_token'})['value']
+login_page_html = BeautifulSoup(login_page.text, "html.parser")
+csrf_token = login_page_html.find("input", {"id": "csrf_token"})["value"]
 
 # Login with CSRF token
-welcome_page = session.post(f'http://superset-external.{namespace}.svc.cluster.local:8088/login/', data={'username': 'admin','password': 'admin', 'csrf_token': csrf_token})
+welcome_page = session.post(
+    f"http://superset-external.{namespace}.svc.cluster.local:8088/login/",
+    data={"username": "admin", "password": "admin", "csrf_token": csrf_token},
+)
 assert welcome_page.status_code == 200
 logging.debug(welcome_page.url)
 
 # Call an API that will trigger an update of user roles
-session.get(f'http://superset-external.{namespace}.svc.cluster.local:8088/api/v1/dashboard/')
+session.get(
+    f"http://superset-external.{namespace}.svc.cluster.local:8088/api/v1/dashboard/"
+)
 
 # Get user roles
-user_roles = set(session.get(f'http://superset-external.{namespace}.svc.cluster.local:8088/api/v1/me/roles/').json()['result']['roles'].keys())
-logging.debug('User roles:     %s', user_roles)
-logging.debug('Expected roles: %s', expected_roles)
+user_roles = set(
+    session.get(
+        f"http://superset-external.{namespace}.svc.cluster.local:8088/api/v1/me/roles/"
+    )
+    .json()["result"]["roles"]
+    .keys()
+)
+logging.debug("User roles:     %s", user_roles)
+logging.debug("Expected roles: %s", expected_roles)
 assert user_roles == expected_roles
--- tests/templates/kuttl/smoke/login.py
+++ tests/templates/kuttl/smoke/login.py
@@ -2,13 +2,18 @@ import requests
 import sys
 import logging
 
-logging.basicConfig(level='DEBUG', format='%(asctime)s %(levelname)s: %(message)s', stream=sys.stdout)
+logging.basicConfig(
+    level="DEBUG", format="%(asctime)s %(levelname)s: %(message)s", stream=sys.stdout
+)
 
-http_code = requests.post("http://superset-node-default:8088/api/v1/security/login", json={
-    "password": "admin",
-    "provider": "db",
-    "refresh": "true",
-    "username": "admin",
-}).status_code
+http_code = requests.post(
+    "http://superset-node-default:8088/api/v1/security/login",
+    json={
+        "password": "admin",
+        "provider": "db",
+        "refresh": "true",
+        "username": "admin",
+    },
+).status_code
 
 assert http_code == 200, "Login failed."
