d0e1b67523c86d8a059d9455e2ee874a06c7a873
Nick <10092581+NickLarsenNZ@users.noreply.github.com>
2025-07-19 00:44:22 +1200

    chore: Release 25.7.0-rc1 (#650)
    Signed-off-by: Nick Larsen <nick.larsen@stackable.tech>

--- Cargo.nix
+++ Cargo.nix
@@ -8631,7 +8631,7 @@ rec {
       };
       "stackable-superset-operator" = rec {
         crateName = "stackable-superset-operator";
-        version = "0.0.0-dev";
+        version = "25.7.0-rc1";
         edition = "2021";
         crateBin = [
           {
