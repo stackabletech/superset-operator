3b5f3c63fbf275ae8e6c1a3743d53b8f4507cc83
Techassi <git@techassi.dev>
2025-11-07 09:58:43 +0100

    chore: Release 25.11.0
    Signed-off-by: Techassi <git@techassi.dev>

--- Cargo.nix
+++ Cargo.nix
@@ -8372,7 +8372,7 @@ rec {
       };
       "stackable-superset-operator" = rec {
         crateName = "stackable-superset-operator";
-        version = "25.11.0-rc1";
+        version = "25.11.0";
         edition = "2021";
         crateBin = [
           {
