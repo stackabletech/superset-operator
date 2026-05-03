234a4a39aa2086a73a623f7ae7c0b87770be4ff0
Malte Sander <malte.sander.it@gmail.com>
2026-05-03 11:43:23 +0200

    fix: add Deployment to watch.

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -12,7 +12,7 @@ use stackable_operator::{
     crd::authentication::core,
     eos::EndOfSupportChecker,
     k8s_openapi::api::{
-        apps::v1::StatefulSet,
+        apps::v1::{Deployment, StatefulSet},
         batch::v1::Job,
         core::v1::{ConfigMap, Service},
     },
@@ -150,6 +150,11 @@ async fn main() -> anyhow::Result<()> {
                 )
                 .owns(
                     watch_namespace.get_api::<DeserializeGuard<StatefulSet>>(&client),
+                    watcher::Config::default(),
+                )
+                // Required for workers and beat.
+                .owns(
+                    watch_namespace.get_api::<DeserializeGuard<Deployment>>(&client),
                     watcher::Config::default(),
                 )
                 .watches(
