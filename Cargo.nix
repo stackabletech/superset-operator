f9ad2f7063503a664d1bab9d962f0a7cfad622e6
Saul Beck <saul.beck@stfc.ac.uk>
2026-06-01 12:34:33 +0100

    chore: bump Rust edition to 2024 and apply cargo fix + clippy updates
    Run `cargo fix --edition` and resolve resulting warnings:
    
    - Fix Rust 2024 temporary drop order change by simplifying `_initial_reconcile_rx` handling

--- Cargo.nix
+++ Cargo.nix
@@ -9936,7 +9936,7 @@ rec {
       "stackable-superset-operator" = rec {
         crateName = "stackable-superset-operator";
         version = "0.0.0-dev";
-        edition = "2021";
+        edition = "2024";
         crateBin = [
           {
             name = "stackable-superset-operator";
